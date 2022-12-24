212f48ed133fab3bff3d0ea69a9c45cb3b40d8c9
Kirill <kirporoh53rus@gmail.com>
2022-12-24 15:04:33 +0300

    django

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls'))
 ]
-if settings.DEBUG: #!
+if settings.DEBUG: #!1
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
