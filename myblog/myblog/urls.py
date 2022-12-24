1a8f5b149bb336744a931a873ad2bdc0217c0449
Kirill <kirporoh53rus@gmail.com>
2022-12-24 15:11:01 +0300

    firstcommit

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls'))
 ]
-if settings.DEBUG: #!1
+if settings.DEBUG: #!12
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
